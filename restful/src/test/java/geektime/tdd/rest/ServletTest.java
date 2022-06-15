a4aa1d7822cbed9d9b7a9dbd5ca95be61b6408ce
zhatian <289460202@qq.com>
2022-06-16 00:06:35 +0800

    3

---
+++ restful/src/test/java/geektime/tdd/rest/ServletTest.java
@@ -0,0 +1,55 @@
+package geektime.tdd.rest;
+
+import jakarta.servlet.Servlet;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public abstract class ServletTest {
+
+    private Server server;
+
+    @BeforeEach
+    void start() throws Exception {
+        server = new Server(8080);
+        Connector connector = new ServerConnector(server);
+        server.addConnector(connector);
+
+        ServletContextHandler handler = new ServletContextHandler(server, "/");
+        handler.addServlet(new ServletHolder(getServlet()), "/");
+        server.setHandler(handler);
+        server.start();
+    }
+
+    @AfterEach
+    void stop() throws Exception {
+        server.stop();
+    }
+
+    protected abstract Servlet getServlet();
+
+    protected URI path(String path) throws Exception {
+        return new URL(new URL("http://localhost:8080/"), path).toURI();
+    }
+
+    protected HttpResponse<String> get(String path) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(path(path)).GET().build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+}
