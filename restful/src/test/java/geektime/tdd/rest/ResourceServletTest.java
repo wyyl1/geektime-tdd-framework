a7aae09879d843e44a3cc7d9c057b0a035a59262
zhatian <289460202@qq.com>
2022-06-17 23:02:25 +0800

    5

--- restful/src/test/java/geektime/tdd/rest/ResourceServletTest.java
+++ restful/src/test/java/geektime/tdd/rest/ResourceServletTest.java
@@ -1,21 +1,26 @@
 package geektime.tdd.rest;
 
 import jakarta.servlet.Servlet;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.container.ResourceContext;
-import jakarta.ws.rs.core.MultivaluedHashMap;
-import jakarta.ws.rs.core.MultivaluedMap;
-import jakarta.ws.rs.core.NewCookie;
-import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.*;
+import jakarta.ws.rs.ext.MessageBodyWriter;
+import jakarta.ws.rs.ext.Providers;
 import jakarta.ws.rs.ext.RuntimeDelegate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 import java.net.http.HttpResponse;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 class ResourceServletTest extends ServletTest{
@@ -23,34 +28,26 @@ class ResourceServletTest extends ServletTest{
     private Runtime runtime;
     private ResourceRouter router;
     private ResourceContext resourceContext;
+    private Providers providers;
+    private OutboundResponseBuilder builder;
 
     @Override
     protected Servlet getServlet() {
         runtime = Mockito.mock(Runtime.class);
         router = Mockito.mock(ResourceRouter.class);
         resourceContext = Mockito.mock(ResourceContext.class);
+        providers = Mockito.mock(Providers.class);
 
         when(runtime.getResourceRouter()).thenReturn(router);
         when(runtime.createResourceContext(any(), any())).thenReturn(resourceContext);
+        when(runtime.getProviders()).thenReturn(providers);
 
         return new ResourceServlet(runtime);
     }
 
-    @Test
-    void should_use_status_from_response() throws Exception {
-        OutboundResponse response = Mockito.mock(OutboundResponse.class);
-        when(response.getStatus()).thenReturn(Response.Status.NOT_MODIFIED.getStatusCode());
-        when(response.getHeaders()).thenReturn(new MultivaluedHashMap<>());
-
-        when(router.dispatch(any(), eq(resourceContext))).thenReturn(response);
-
-        HttpResponse<String> httpResponse = get("/test");
-
-        assertEquals(Response.Status.NOT_MODIFIED.getStatusCode(), httpResponse.statusCode());
-    }
-
-    @Test
-    void should_use_http_headers_from_response() throws Exception {
+    @BeforeEach
+    void before() {
+        builder = new OutboundResponseBuilder();
         RuntimeDelegate delegate = Mockito.mock(RuntimeDelegate.class);
         RuntimeDelegate.setInstance(delegate);
         when(delegate.createHeaderDelegate(eq(NewCookie.class))).thenReturn(new RuntimeDelegate.HeaderDelegate<NewCookie>() {
@@ -64,22 +61,83 @@ class ResourceServletTest extends ServletTest{
                 return value.getName() + "=" + value.getValue();
             }
         });
+    }
 
-        NewCookie sessionId = new NewCookie.Builder("SESSION_ID").value("session").build();
-        NewCookie userId = new NewCookie.Builder("USER_ID").value("user").build();
+    @Test
+    void should_use_status_from_response() throws Exception {
+        builder.status(Response.Status.NOT_MODIFIED).build(router);
 
-        OutboundResponse response = Mockito.mock(OutboundResponse.class);
+        HttpResponse<String> httpResponse = get("/test");
 
-        MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
-        headers.addAll("Set-Cookie", sessionId, userId);
+        assertEquals(Response.Status.NOT_MODIFIED.getStatusCode(), httpResponse.statusCode());
+    }
 
-        when(response.getStatus()).thenReturn(Response.Status.NOT_MODIFIED.getStatusCode());
-        when(response.getHeaders()).thenReturn(headers);
-
-        when(router.dispatch(any(), eq(resourceContext))).thenReturn(response);
+    @Test
+    void should_use_http_headers_from_response() throws Exception {
+        builder.headers("Set-Cookie", new NewCookie.Builder("SESSION_ID").value("session").build(),
+                new NewCookie.Builder("USER_ID").value("user").build()).build(router);
 
         HttpResponse<String> httpResponse = get("/test");
 
         assertArrayEquals(new String[]{"SESSION_ID=session", "USER_ID=user"}, httpResponse.headers().allValues("Set-Cookie").toArray(String[]::new));
     }
+
+    @Test
+    void should_write_entity_to_http_response_using_message_body_writer() throws Exception {
+        builder.entity(new GenericEntity<>("entity", String.class), new Annotation[0]).build(router);
+
+        HttpResponse<String> httpResponse = get("/test");
+
+        assertEquals("entity", httpResponse.body());
+    }
+
+    class OutboundResponseBuilder {
+        Response.Status status = Response.Status.OK;
+        MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
+        GenericEntity<String> entity = new GenericEntity<>("entity", String.class);
+        Annotation[] annotations = new Annotation[0];
+        MediaType mediaType = MediaType.TEXT_PLAIN_TYPE;
+
+        public OutboundResponseBuilder status(Response.Status status) {
+            this.status = status;
+            return this;
+        }
+
+        public OutboundResponseBuilder headers(String name, Object... values) {
+            headers.addAll(name, values);
+            return this;
+        }
+
+        public OutboundResponseBuilder entity(GenericEntity<String> entity, Annotation[] annotations) {
+            this.entity = entity;
+            this.annotations = annotations;
+            return this;
+        }
+
+        void build(ResourceRouter router) {
+            OutboundResponse response = Mockito.mock(OutboundResponse.class);
+            when(response.getStatus()).thenReturn(status.getStatusCode());
+            when(response.getHeaders()).thenReturn(headers);
+            when(response.getGenericEntity()).thenReturn(entity);
+            when(response.getAnnotations()).thenReturn(annotations);
+            when(response.getMediaType()).thenReturn(mediaType);
+            when(router.dispatch(any(), eq(resourceContext))).thenReturn(response);
+
+            when(providers.getMessageBodyWriter(eq(String.class), eq(String.class), same(annotations), eq(mediaType)))
+                    .thenReturn(new MessageBodyWriter<>() {
+                        @Override
+                        public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+                            return false;
+                        }
+
+                        @Override
+                        public void writeTo(String s, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+                            PrintWriter writer = new PrintWriter(entityStream);
+                            writer.write(s);
+                            writer.flush();
+                        }
+                    });
+        }
+    }
+
 }
